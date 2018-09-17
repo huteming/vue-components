3ec116753cf7583c59decef3d080248e25e7729d
Tommy <tommy@jhshuyuan.com>
2018-09-17 16:51:47 +0800

    Updates

--- precache-manifest.e8f2b5beea825ed6a4f6a6ae23858fa0.js
+++ precache-manifest.e8f2b5beea825ed6a4f6a6ae23858fa0.js
@@ -4,60 +4,60 @@ self.__precacheManifest = [
     "url": "/vue-components/js/chunk-3af58446.e95bebf8.js"
   },
   {
+    "revision": "9379f50040087f12bcd5aa5b7d10bb49",
+    "url": "/vue-components/img/big.png"
+  },
+  {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/vue-components/robots.txt"
   },
   {
-    "revision": "8c0695156f56839c29a9",
-    "url": "/vue-components/css/chunk-4550a5bb.9da04fbc.css"
-  },
-  {
     "revision": "bda54b95ef7d6bbc5e6a",
-    "url": "/vue-components/css/chunk-07dfe172.0e3158e3.css"
+    "url": "/vue-components/js/chunk-07dfe172.f7271ea7.js"
   },
   {
-    "revision": "e6cee89de8a762e43206",
-    "url": "/vue-components/css/chunk-0d6d8b85.fe063a9d.css"
+    "revision": "306ebf3680236edfc62f2254b9fce7b2",
+    "url": "/vue-components/js/qrcode.js"
   },
   {
     "revision": "e6cee89de8a762e43206",
     "url": "/vue-components/js/chunk-0d6d8b85.8978afcc.js"
   },
   {
-    "revision": "d136339ea963e94f7469",
-    "url": "/vue-components/css/chunk-123d7b8b.f3c2763e.css"
+    "revision": "35f627f6c7f15fdd722626f981759156",
+    "url": "/vue-components/js/qrcode.md"
   },
   {
     "revision": "d136339ea963e94f7469",
     "url": "/vue-components/js/chunk-123d7b8b.967dd9fd.js"
   },
   {
-    "revision": "f813bdaa552eed6c15c0",
-    "url": "/vue-components/css/chunk-15a3a597.3025667d.css"
+    "revision": "8c7380a5600b304f3424fa8971187ba3",
+    "url": "/vue-components/js/jweixin-1.2.0.js"
   },
   {
     "revision": "f813bdaa552eed6c15c0",
     "url": "/vue-components/js/chunk-15a3a597.b4e424ea.js"
   },
   {
-    "revision": "748d0cbd09a99bd0879d",
-    "url": "/vue-components/css/chunk-21546392.1d79d6db.css"
+    "revision": "a17a1e8624e9781e71f24dfd393020fd",
+    "url": "/vue-components/img/imageConversion.png"
   },
   {
     "revision": "748d0cbd09a99bd0879d",
     "url": "/vue-components/js/chunk-21546392.e70fbcb3.js"
   },
   {
-    "revision": "54044b65095cae2532a8",
-    "url": "/vue-components/css/chunk-2415943d.58a8757e.css"
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-components/img/logo.png"
   },
   {
     "revision": "54044b65095cae2532a8",
     "url": "/vue-components/js/chunk-2415943d.02d2e198.js"
   },
   {
-    "revision": "c414f98b79f39d88e89f",
-    "url": "/vue-components/css/chunk-2a1087ad.21a384cc.css"
+    "revision": "71603a1b3853ddb516b35d4f17ea12f7",
+    "url": "/vue-components/img/dengken.jpg"
   },
   {
     "revision": "c414f98b79f39d88e89f",
@@ -176,235 +176,235 @@ self.__precacheManifest = [
     "url": "/vue-components/js/chunk-2d2315e6.c8964aa0.js"
   },
   {
-    "revision": "d079f8c614c3b57304ad",
-    "url": "/vue-components/css/chunk-2dc3c6f7.7d09090e.css"
+    "revision": "3af34619f31acf54021c1f9edd912b03",
+    "url": "/vue-components/img/html5.png"
   },
   {
     "revision": "d079f8c614c3b57304ad",
     "url": "/vue-components/js/chunk-2dc3c6f7.2abe1bb6.js"
   },
   {
-    "revision": "110a30328e7d2ca207ab",
-    "url": "/vue-components/css/chunk-308ab9f4.ceeed9ab.css"
+    "revision": "6a0e9b60dc70284c211fb5210616b907",
+    "url": "/vue-components/index.html"
   },
   {
     "revision": "110a30328e7d2ca207ab",
     "url": "/vue-components/js/chunk-308ab9f4.9cd3b3cb.js"
   },
   {
-    "revision": "fea0c73545d8bf691fc8",
-    "url": "/vue-components/css/chunk-355b7000.2fd5519b.css"
+    "revision": "5909f667d77fc68679367f6aef781422",
+    "url": "/vue-components/img/phone.5909f667.png"
   },
   {
     "revision": "fea0c73545d8bf691fc8",
     "url": "/vue-components/js/chunk-355b7000.56649c34.js"
   },
   {
-    "revision": "8347f3bd3e0c72b8c758",
-    "url": "/vue-components/css/chunk-3af58446.c258af61.css"
+    "revision": "936eac074a9dd35601ad80b3c210f17e",
+    "url": "/vue-components/img/iconfont.936eac07.svg"
   },
   {
-    "revision": "8f1347a74e6c900bf1e0",
-    "url": "/vue-components/js/app.6f465aa0.js"
-  },
-  {
-    "revision": "bda54b95ef7d6bbc5e6a",
-    "url": "/vue-components/js/chunk-07dfe172.f7271ea7.js"
-  },
-  {
-    "revision": "8c0695156f56839c29a9",
-    "url": "/vue-components/js/chunk-4550a5bb.80a581dd.js"
-  },
-  {
-    "revision": "8a0303aeac2e7511384e",
-    "url": "/vue-components/css/chunk-55134af3.09e9df3e.css"
-  },
-  {
-    "revision": "8a0303aeac2e7511384e",
-    "url": "/vue-components/js/chunk-55134af3.a6266ce0.js"
-  },
-  {
-    "revision": "fbc4700b9b9d9324e19a",
-    "url": "/vue-components/css/chunk-5b18ffd6.f6f19a79.css"
-  },
-  {
-    "revision": "fbc4700b9b9d9324e19a",
-    "url": "/vue-components/js/chunk-5b18ffd6.125d7bdd.js"
-  },
-  {
-    "revision": "e6c6d59f96cc3ee3ce9c",
-    "url": "/vue-components/css/chunk-60cc6d80.1e12946f.css"
-  },
-  {
-    "revision": "e6c6d59f96cc3ee3ce9c",
-    "url": "/vue-components/js/chunk-60cc6d80.e7526b71.js"
-  },
-  {
-    "revision": "20ad4f31397358be0132",
-    "url": "/vue-components/css/chunk-6408a0c5.4ca89948.css"
-  },
-  {
-    "revision": "20ad4f31397358be0132",
-    "url": "/vue-components/js/chunk-6408a0c5.bbf23d43.js"
-  },
-  {
-    "revision": "447dd64d91cd021f5a81",
-    "url": "/vue-components/css/chunk-6d716564.727e6ea0.css"
-  },
-  {
-    "revision": "447dd64d91cd021f5a81",
-    "url": "/vue-components/js/chunk-6d716564.a08b1084.js"
-  },
-  {
-    "revision": "97240583b1a512417bf4",
-    "url": "/vue-components/css/chunk-706a2768.6a8bc908.css"
-  },
-  {
-    "revision": "97240583b1a512417bf4",
-    "url": "/vue-components/js/chunk-706a2768.9372d800.js"
-  },
-  {
-    "revision": "8a0133a87a811c067fea",
-    "url": "/vue-components/css/chunk-7ac847a4.187d0954.css"
-  },
-  {
-    "revision": "8a0133a87a811c067fea",
-    "url": "/vue-components/js/chunk-7ac847a4.de5867f5.js"
-  },
-  {
-    "revision": "24924158dfe5d7596ec6",
-    "url": "/vue-components/css/chunk-7c0d30dc.aee705c4.css"
-  },
-  {
-    "revision": "24924158dfe5d7596ec6",
-    "url": "/vue-components/js/chunk-7c0d30dc.0c5fde11.js"
-  },
-  {
-    "revision": "6b3ea13a4556c382ba07",
-    "url": "/vue-components/css/chunk-9fbfee7e.269fdfcb.css"
-  },
-  {
-    "revision": "6b3ea13a4556c382ba07",
-    "url": "/vue-components/js/chunk-9fbfee7e.febae6c7.js"
-  },
-  {
-    "revision": "2e7e07d023b60ed3ce6f",
-    "url": "/vue-components/css/chunk-af39e820.fe74f809.css"
-  },
-  {
-    "revision": "2e7e07d023b60ed3ce6f",
-    "url": "/vue-components/js/chunk-af39e820.35c90d0e.js"
-  },
-  {
-    "revision": "12ecc64c3ddacb89fa63",
-    "url": "/vue-components/css/chunk-d6651e62.654e47a6.css"
-  },
-  {
-    "revision": "12ecc64c3ddacb89fa63",
-    "url": "/vue-components/js/chunk-d6651e62.6e5f9ab6.js"
-  },
-  {
-    "revision": "e5788b2eea5bb2fc6e4d",
-    "url": "/vue-components/css/chunk-e178fdea.a0832f0e.css"
-  },
-  {
-    "revision": "e5788b2eea5bb2fc6e4d",
-    "url": "/vue-components/js/chunk-e178fdea.c8569afd.js"
-  },
-  {
-    "revision": "1436cfa0cc67eeea504a",
-    "url": "/vue-components/css/chunk-f5a1b88c.c65955ce.css"
-  },
-  {
-    "revision": "1436cfa0cc67eeea504a",
-    "url": "/vue-components/js/chunk-f5a1b88c.f0d8dded.js"
-  },
-  {
-    "revision": "8988ce1c355d6edc6b7a",
-    "url": "/vue-components/css/chunk-vendors.a4f21b13.css"
-  },
-  {
-    "revision": "8988ce1c355d6edc6b7a",
-    "url": "/vue-components/js/chunk-vendors.e64d3bd0.js"
-  },
-  {
-    "revision": "0813b32f64063e91f1b150323f404511",
-    "url": "/vue-components/fonts/icon.0813b32f.woff"
+    "revision": "dbd2cb5b5b3f437975d2",
+    "url": "/vue-components/js/app.15975fb7.js"
   },
   {
     "revision": "0a7dca3e1c124787894bbbe5a7c9407b",
     "url": "/vue-components/img/icon.0a7dca3e.svg"
   },
   {
-    "revision": "e90b7fe95db62ee1c2e426645494e7db",
-    "url": "/vue-components/fonts/icon.e90b7fe9.eot"
+    "revision": "8c0695156f56839c29a9",
+    "url": "/vue-components/js/chunk-4550a5bb.80a581dd.js"
+  },
+  {
+    "revision": "8988ce1c355d6edc6b7a",
+    "url": "/vue-components/js/chunk-vendors.e64d3bd0.js"
+  },
+  {
+    "revision": "8a0303aeac2e7511384e",
+    "url": "/vue-components/js/chunk-55134af3.a6266ce0.js"
+  },
+  {
+    "revision": "1436cfa0cc67eeea504a",
+    "url": "/vue-components/js/chunk-f5a1b88c.f0d8dded.js"
+  },
+  {
+    "revision": "fbc4700b9b9d9324e19a",
+    "url": "/vue-components/js/chunk-5b18ffd6.125d7bdd.js"
+  },
+  {
+    "revision": "e5788b2eea5bb2fc6e4d",
+    "url": "/vue-components/js/chunk-e178fdea.c8569afd.js"
+  },
+  {
+    "revision": "e6c6d59f96cc3ee3ce9c",
+    "url": "/vue-components/js/chunk-60cc6d80.e7526b71.js"
+  },
+  {
+    "revision": "12ecc64c3ddacb89fa63",
+    "url": "/vue-components/js/chunk-d6651e62.6e5f9ab6.js"
+  },
+  {
+    "revision": "20ad4f31397358be0132",
+    "url": "/vue-components/js/chunk-6408a0c5.bbf23d43.js"
+  },
+  {
+    "revision": "2e7e07d023b60ed3ce6f",
+    "url": "/vue-components/js/chunk-af39e820.35c90d0e.js"
+  },
+  {
+    "revision": "447dd64d91cd021f5a81",
+    "url": "/vue-components/js/chunk-6d716564.a08b1084.js"
+  },
+  {
+    "revision": "6b3ea13a4556c382ba07",
+    "url": "/vue-components/js/chunk-9fbfee7e.febae6c7.js"
+  },
+  {
+    "revision": "97240583b1a512417bf4",
+    "url": "/vue-components/js/chunk-706a2768.9372d800.js"
+  },
+  {
+    "revision": "24924158dfe5d7596ec6",
+    "url": "/vue-components/js/chunk-7c0d30dc.0c5fde11.js"
+  },
+  {
+    "revision": "8a0133a87a811c067fea",
+    "url": "/vue-components/js/chunk-7ac847a4.de5867f5.js"
   },
   {
     "revision": "7ed45bdacbaf8351fb9e1e93e76f105c",
     "url": "/vue-components/fonts/icon.7ed45bda.ttf"
   },
   {
-    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
-    "url": "/vue-components/fonts/element-icons.6f0a7632.ttf"
+    "revision": "24924158dfe5d7596ec6",
+    "url": "/vue-components/css/chunk-7c0d30dc.aee705c4.css"
   },
   {
-    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
-    "url": "/vue-components/fonts/element-icons.2fad952a.woff"
-  },
-  {
-    "revision": "05b2a99e9fd3433fdcdeeeda560e888c",
-    "url": "/vue-components/fonts/iconfont.05b2a99e.ttf"
+    "revision": "6b3ea13a4556c382ba07",
+    "url": "/vue-components/css/chunk-9fbfee7e.269fdfcb.css"
   },
   {
     "revision": "fe4b155790012d7d64b140cabdc483e4",
     "url": "/vue-components/fonts/iconfont.fe4b1557.eot"
   },
   {
-    "revision": "936eac074a9dd35601ad80b3c210f17e",
-    "url": "/vue-components/img/iconfont.936eac07.svg"
+    "revision": "2e7e07d023b60ed3ce6f",
+    "url": "/vue-components/css/chunk-af39e820.fe74f809.css"
   },
   {
-    "revision": "5909f667d77fc68679367f6aef781422",
-    "url": "/vue-components/img/phone.5909f667.png"
+    "revision": "05b2a99e9fd3433fdcdeeeda560e888c",
+    "url": "/vue-components/fonts/iconfont.05b2a99e.ttf"
   },
   {
-    "revision": "809405c15743d9a89dcf2efe04f355fc",
-    "url": "/vue-components/index.html"
+    "revision": "12ecc64c3ddacb89fa63",
+    "url": "/vue-components/css/chunk-d6651e62.654e47a6.css"
   },
   {
-    "revision": "3af34619f31acf54021c1f9edd912b03",
-    "url": "/vue-components/img/html5.png"
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/vue-components/fonts/element-icons.2fad952a.woff"
   },
   {
-    "revision": "71603a1b3853ddb516b35d4f17ea12f7",
-    "url": "/vue-components/img/dengken.jpg"
+    "revision": "e5788b2eea5bb2fc6e4d",
+    "url": "/vue-components/css/chunk-e178fdea.a0832f0e.css"
   },
   {
-    "revision": "9379f50040087f12bcd5aa5b7d10bb49",
-    "url": "/vue-components/img/big.png"
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/vue-components/fonts/element-icons.6f0a7632.ttf"
   },
   {
-    "revision": "a17a1e8624e9781e71f24dfd393020fd",
-    "url": "/vue-components/img/imageConversion.png"
+    "revision": "1436cfa0cc67eeea504a",
+    "url": "/vue-components/css/chunk-f5a1b88c.c65955ce.css"
   },
   {
-    "revision": "82b9c7a5a3f405032b1db71a25f67021",
-    "url": "/vue-components/img/logo.png"
+    "revision": "e90b7fe95db62ee1c2e426645494e7db",
+    "url": "/vue-components/fonts/icon.e90b7fe9.eot"
   },
   {
-    "revision": "8c7380a5600b304f3424fa8971187ba3",
-    "url": "/vue-components/js/jweixin-1.2.0.js"
+    "revision": "8988ce1c355d6edc6b7a",
+    "url": "/vue-components/css/chunk-vendors.a4f21b13.css"
   },
   {
-    "revision": "35f627f6c7f15fdd722626f981759156",
-    "url": "/vue-components/js/qrcode.md"
+    "revision": "0813b32f64063e91f1b150323f404511",
+    "url": "/vue-components/fonts/icon.0813b32f.woff"
   },
   {
-    "revision": "306ebf3680236edfc62f2254b9fce7b2",
-    "url": "/vue-components/js/qrcode.js"
+    "revision": "110a30328e7d2ca207ab",
+    "url": "/vue-components/css/chunk-308ab9f4.ceeed9ab.css"
   },
   {
-    "revision": "8f1347a74e6c900bf1e0",
+    "revision": "8a0303aeac2e7511384e",
+    "url": "/vue-components/css/chunk-55134af3.09e9df3e.css"
+  },
+  {
+    "revision": "fbc4700b9b9d9324e19a",
+    "url": "/vue-components/css/chunk-5b18ffd6.f6f19a79.css"
+  },
+  {
+    "revision": "8a0133a87a811c067fea",
+    "url": "/vue-components/css/chunk-7ac847a4.187d0954.css"
+  },
+  {
+    "revision": "e6c6d59f96cc3ee3ce9c",
+    "url": "/vue-components/css/chunk-60cc6d80.1e12946f.css"
+  },
+  {
+    "revision": "20ad4f31397358be0132",
+    "url": "/vue-components/css/chunk-6408a0c5.4ca89948.css"
+  },
+  {
+    "revision": "447dd64d91cd021f5a81",
+    "url": "/vue-components/css/chunk-6d716564.727e6ea0.css"
+  },
+  {
+    "revision": "97240583b1a512417bf4",
+    "url": "/vue-components/css/chunk-706a2768.6a8bc908.css"
+  },
+  {
+    "revision": "748d0cbd09a99bd0879d",
+    "url": "/vue-components/css/chunk-21546392.1d79d6db.css"
+  },
+  {
+    "revision": "8347f3bd3e0c72b8c758",
+    "url": "/vue-components/css/chunk-3af58446.c258af61.css"
+  },
+  {
+    "revision": "8c0695156f56839c29a9",
+    "url": "/vue-components/css/chunk-4550a5bb.9da04fbc.css"
+  },
+  {
+    "revision": "fea0c73545d8bf691fc8",
+    "url": "/vue-components/css/chunk-355b7000.2fd5519b.css"
+  },
+  {
+    "revision": "d079f8c614c3b57304ad",
+    "url": "/vue-components/css/chunk-2dc3c6f7.7d09090e.css"
+  },
+  {
+    "revision": "c414f98b79f39d88e89f",
+    "url": "/vue-components/css/chunk-2a1087ad.21a384cc.css"
+  },
+  {
+    "revision": "54044b65095cae2532a8",
+    "url": "/vue-components/css/chunk-2415943d.58a8757e.css"
+  },
+  {
+    "revision": "f813bdaa552eed6c15c0",
+    "url": "/vue-components/css/chunk-15a3a597.3025667d.css"
+  },
+  {
+    "revision": "d136339ea963e94f7469",
+    "url": "/vue-components/css/chunk-123d7b8b.f3c2763e.css"
+  },
+  {
+    "revision": "e6cee89de8a762e43206",
+    "url": "/vue-components/css/chunk-0d6d8b85.fe063a9d.css"
+  },
+  {
+    "revision": "bda54b95ef7d6bbc5e6a",
+    "url": "/vue-components/css/chunk-07dfe172.0e3158e3.css"
+  },
+  {
+    "revision": "dbd2cb5b5b3f437975d2",
     "url": "/vue-components/css/app.0f21fdbc.css"
   }
 ];
