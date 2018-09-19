b85935864a4c1591d2147e2fb403eadcc1e4d1f2
Tommy <tommy@jhshuyuan.com>
2018-09-19 13:18:29 +0800

    eslint add rule comma-dangle

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  globals: {
-    wx: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': [2, 4]
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    globals: {
+        wx: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    rules: {
+        // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': ['error', 4],
+        'comma-dangle': ['error', {
+            'arrays': 'always-multiline',
+            'objects': 'always-multiline',
+            'imports': 'never',
+            'exports': 'never',
+            'functions': 'ignore',
+        }]
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 }
--- apps/App.vue
+++ apps/App.vue
@@ -34,7 +34,7 @@ export default {
 
     mounted () {
         this.$tools.wx.saveURL(location.href)
-    }
+    },
 }
 </script>
 
